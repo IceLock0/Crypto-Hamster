27036ee6bfaf35ce3acaefbf056165e2501d0a0e
icelock <makeew.evgenij-2003@mail.ru>
2024-07-30 14:05:51 +0300

    added electricity (bar decreaser)

---
+++ Assets/Scripts/Presenters/Electricity/ElectricityPresenter.cs
@@ -0,0 +1,47 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Views.UI.Electricity;
+
+namespace Presenters.Electricity
+{
+    public class ElectricityPresenter
+    {
+        private readonly ElectricityUIBarView _view;
+
+        public ElectricityPresenter(ElectricityUIBarView view)
+        {
+            _view = view;
+
+            _currentElectricity = _maxElectricity;
+            
+            DecreaseElectricity();
+        }
+        
+        private float _maxElectricity = 100.0f;
+        private float _currentElectricity;
+        private float _secondsDelay = 1.0f;
+        private float _decreaseValue = 5.0f;
+
+        private void SetCurrentElectricity(float value)
+        {
+            _currentElectricity = Mathf.Clamp(value, 0, _maxElectricity);
+
+            Debug.Log($"{_currentElectricity}");
+            
+            if(_currentElectricity <= 0) _view.ShowNotification();
+            else _view.HideNotification();
+        }
+        
+        private async UniTaskVoid DecreaseElectricity()
+        {
+            while (true)
+            {
+                await UniTask.Delay((int) _secondsDelay * 1000);
+                
+                SetCurrentElectricity(_currentElectricity - _decreaseValue);
+                
+                _view.ChangeFillAmount(_currentElectricity / _maxElectricity, _secondsDelay * 2);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Views/UI/Electricity/ElectricityUIBarView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Presenters.Electricity;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Views.UI.Electricity
+{
+    public class ElectricityUIBarView : MonoBehaviour
+    {
+        [SerializeField] private Image _electricityImage;
+        [SerializeField] private GameObject _noElectricityNotification;
+
+        private ElectricityPresenter _presenter;
+        
+        public void ChangeFillAmount(float value, float duration)
+        {
+            _electricityImage.DOFillAmount(value, duration);
+        }
+
+        public void ShowNotification()
+        {
+            _noElectricityNotification.SetActive(true);
+        }
+
+        public void HideNotification()
+        {
+            _noElectricityNotification.SetActive(false);
+        }
+        
+        private void Awake()
+        {
+            InitStartValues();
+            
+            _presenter = new ElectricityPresenter(this);
+        }
+
+        private void InitStartValues()
+        {
+            _electricityImage.gameObject.SetActive(true);
+            _electricityImage.fillAmount = 1.0f;
+            
+            _noElectricityNotification.gameObject.SetActive(false);
+        }
+    }
+}
