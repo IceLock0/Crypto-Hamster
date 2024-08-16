2fed7c790a5dd9b715f39f0078fbd9bcb9a9088a
icelock <makeew.evgenij-2003@mail.ru>
2024-08-16 12:49:44 +0300

    base ui image, shortcut

---
+++ Assets/Scripts/Views/Phone/Staff/PhoneStaffButtonView.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneStaffButtonView : MonoBehaviour
+{
+    [SerializeField] private GameObject _staffPanelGO;
+    
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(ChangeStaffPanelVisibility);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(ChangeStaffPanelVisibility);
+    }
+
+    private void ChangeStaffPanelVisibility()
+    {
+        _staffPanelGO.SetActive(!_staffPanelGO.activeSelf);
+    }
+}
